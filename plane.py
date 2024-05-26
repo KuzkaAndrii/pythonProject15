658c5df45e7ecbc408c657a7f3bedc33e4043f33
KuzkaAndrii <157294020+KuzkaAndrii@users.noreply.github.com>
2024-05-26 21:49:09 +0300

    plane can flight

--- plane.py
+++ plane.py
@@ -1,8 +1,18 @@
 from programobgect import *
 class Plain(PObj):
     def __init__(self, fon, x, y, r):
+        self._genpos=x
         super().__init__(fon, x, y, r)
         self._plane=self._fon.create_polygon(self._x, self._y, self._x+0.3*self._r, self._y-0.1*self._r, self._x+0.3*self._r, self._y+self._r*0.1, self._x, self._y, fill="green")
+        self._fon.after(45, self.move, self._plane)
+    def move(self, p):
+        self._x-=5
+        if self._x<=0:
+            self._fon.move(p, abs(self._x-self._genpos), 0)
+            self._x=self._genpos
+        self._fon.move(p, -5, 0)
+        self._fon.after(45, self.move, p)
+
 
 if __name__=="__main__":
     print("plain")
