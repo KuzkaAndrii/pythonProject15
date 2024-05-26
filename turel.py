887b9e052ff4024df94c0eea117087e279bd65b8
KuzkaAndrii <157294020+KuzkaAndrii@users.noreply.github.com>
2024-05-26 21:31:21 +0300

    created center

--- turel.py
+++ turel.py
@@ -1,9 +1,17 @@
 from programobgect import *
+from math import sin, cos
+
 class Head(PObj):
     def __init__(self, fon, x, y, r):
-        super().__init__(fon, x, y, r)
-        self.truba=self._fon.create_polygon(x+1.25*r, y+r*0.03, x+0.5*r, y+r*0.03, x+0.5*r, y-r*0.02, x+1.25*r, y-r*0.02, fill="grey", tag="stvol")
-        self.hole = self._fon.create_oval(x + 1.2 * r, y + r * 0.03, x + 1.3 * r, y - r * 0.02, fill="black", tag="stvol")
+        super().__init__(fon, x+0.5*r, y+r*0.005, r)
+        self.truba=self._fon.create_polygon(self._x+0.75*self._r, self._y+self._r*0.025, self._x, self._y+self._r*0.03, self._x, self._y-self._r*0.025, self._x+0.75*self._r, self._y-self._r*0.025, fill="grey", tag="stvol")
+        self.hole = self._fon.create_oval(self._x+0.7*self._r, self._y+self._r*0.025, self._x+0.8*self._r, self._y-self._r*0.025, fill="black", tag="stvol")
+    def muve(self, ar):
+        co=cos(-1*ar)
+        si=sin(-1*ar)
+
+
+
 class Turel(PObj):
     def __init__(self, fon, x, y, r):
         super().__init__(fon, x, y, r)
@@ -12,6 +20,7 @@ class Turel(PObj):
     def piw_paw(self):
         pass
     def muve(self):
-        pass
+        ar=1
+        self._stvol.muve(ar)
 if __name__=="__main__":
     print("turel")
